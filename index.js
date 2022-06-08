9adcc39b67c202401f6c986e7c2024fc7cc40b3b
unknown <mdtanbir576@gmail.com>
2022-06-08 15:50:42 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
-import express from "express";
+
+const express = require("express")
+
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+app.use(express.json())
+app.use(cors());
 
 
 
@@ -9,6 +17,21 @@ app.get("/", (req,res) => {
     res.send("server is running")
 })
 
+
+
+const connect = async function main(){
+    try {
+      await mongoose.connect(process.env.MONGO);
+       console.log("Connected to backend");
+    } catch (error) {
+     
+    }
+
+}
+
+
+
 app.listen(7000, () => {
+    connect();
     console.log("server is running");
 })
