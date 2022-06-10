0b8c3acb5642a2e32b146bebd0b91aece433a1a9
unknown <mdtanbir576@gmail.com>
2022-06-10 21:28:51 +0600

    Hotel Schema

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const express = require("express")
 
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+const hotelRoute = require("./routes/hotels");
+const roomsRoute = require("./routes/rooms");
+const usersRoute = require("./routes/users");
+
 require("dotenv").config();
 
 const app = express();
 
+
+
+// middleware
 app.use(express.json())
 app.use(cors());
 
 
+
+app.use("api/auth", authRoute)
+app.use("api/hotels", hotelRoute);
+app.use("api/auth", roomRoute);
+app.use("api/auth", usersRoute);
 
 app.get("/", (req,res) => {
     res.send("server is running")
@@ -24,12 +38,19 @@ const connect = async function main(){
       await mongoose.connect(process.env.MONGO);
        console.log("Connected to backend");
     } catch (error) {
+        throw error;
      
     }
 
 }
 
+mongoose.connection.on("diconnected", () => {
+    console.log("mongoDB disconnected")
+})
 
+mongoose.connection.on("connected", () => {
+    console.log("connected")
+})
 
 app.listen(7000, () => {
     connect();
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const router = express.Router();
+
+
+router.get("/", (req,res) => {
+    res.send("Hello, this is auth endpoint");
+})
+
+
+module.exports = router;
