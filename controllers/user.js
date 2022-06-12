80af2df4dcb134a41cc6b6ef16ea49aff360e271
unknown <mdtanbir576@gmail.com>
2022-06-12 17:30:00 +0600

    User Schema

---
+++ controllers/user.js
@@ -0,0 +1,44 @@
+import User from "../models/User";
+
+
+//
+
+export const updateuser = async (req, res, next) => {
+  try {
+    const updateduser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updateduser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteuser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("user has been deleted");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getuser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAlluser = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+};
