474b2dc447ba6da46d49a9a309a887752192732a
unknown <mdtanbir576@gmail.com>
2022-06-13 14:17:12 +0600

    hotel room functionality

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,10 +1,30 @@
 
 import express from "express";
+import { createRoom, deleteRoom, getAllRoom, getRoom, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, this is rooms endpoint");
-});
 
+
+
+// create
+
+router.post("/:hotelId", verifyAdmin, createRoom) 
+
+// update
+
+router.put("/:id",verifyAdmin, updateRoom)
+
+// delete
+
+router.delete("/:id/:hotelId",verifyAdmin, deleteRoom)
+
+// gettin a specific hotel
+
+router.get("/:id", getRoom)
+
+// getting all hotel
+
+router.get("/",getAllRoom)
 export default router;
